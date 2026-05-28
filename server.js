79f4045ff53816c7637c85161ad3046473eee08c
minhhai1356tq-sudo <minhhai1356tq@gmail.com>
2026-05-28 23:04:17 +0700

    Change URL for key 'hai2000' in server.js
    Updated the URL for the key 'hai2000' in the keys object.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const keys = {
-  "hai2000": "https://pastefy.app/mG1jHL7h/raw",
+  "hai2000": "https://pastefy.app/YwHV4roh",
   "VIP999": "https://pastefy.app/TMlop60f/raw"
 };
 
