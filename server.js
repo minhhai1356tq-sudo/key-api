ce8424fe4453a275d5828d50f2aadc5d6be7bb28
minhhai1356tq-sudo <minhhai1356tq@gmail.com>
2026-05-28 22:04:03 +0700

    Update API key in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const keys = {
-  "3636": "https://orrxl4-protector.com/api/raw?id=rs1olsb3",
+  "hai2000": "https://orrxl4-protector.com/api/raw?id=rs1olsb3",
   "VIP999": "https://pastefy.app/TMlop60f/raw"
 };
 
